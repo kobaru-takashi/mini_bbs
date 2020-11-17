e79bb2b4324bd11ba397ac8b4086eb30681017f0
kobaru-takashi <ta.a.1445.1@gmail.com>
2020-11-17 11:28:17 +0900

    ログインページ

--- join/index.php
+++ join/index.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+require('../dbconnect.php');
+
 if (!empty($_POST)) {
 		if ($_POST['name'] === '') {
 				$error['name'] = 'blank';
@@ -19,6 +21,16 @@ if (!empty($_POST)) {
 			if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png') {
 					$error['image'] = 'type';
 			}
+		}
+
+		//アカウントの重複チェック
+		if (empty($error)) {
+				$member = $db->prepare('SELECT COUNT(*) AS cnt FROM members  WHERE email=?');
+				$member->execute(array($_POST['email']));
+				$record = $member->fetch();
+				if ($record['cnt'] > 0) {
+						$error['email'] = 'duplicate';
+				}
 		}
 
 		if (empty($error)) {
@@ -67,7 +79,10 @@ if ($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])) {
 		<dd>
         	<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)); ?>" />
 					<?php if ($error['email'] === 'blank'): ?>
-					<p class='"error">* メールアドレスを入力して下さい</p>
+					<p class="error">* メールアドレスを入力して下さい</p>
+					<?php endif; ?>
+					<?php if ($error['email'] === 'duplicate'): ?>
+					<p class="error">* 指定されたメールアドレスは、すでに登録されています</p>
 					<?php endif; ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
